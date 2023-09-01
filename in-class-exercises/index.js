9dc532ee57e12a604cca45cf5568485a3d17d64e
jugglingdev <kaylamarie1785@gmail.com>
2023-08-31 20:10:44 -0500

    Complete Exercise 16

--- in-class-exercises/index.js
+++ in-class-exercises/index.js
@@ -116,12 +116,27 @@ reverseWords('Can you hear me now?');
 // Exercise 14
 
 // function balancedParentheses(string) {
+//     let stack = [];
 
 // }
 
 // Exercise 15
 
 // Exercise 16
+
+function FizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 == 0) {
+            console.log('Fizz');
+        } else if (i % 5 == 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 // Exercise 17
 
